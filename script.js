cc3bc92ce81b3e10bd92850e9153ffa52c8982bb
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2022-07-28 15:10:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-const person = {
-  one: function() {alert("one")},
-  two: function() {alert("2")},
-  three: function() {alert("three")},
-  four: function() {alert("4")}
-};
-person.one;
-person.two;
-person.three;
-person.four;
+function setCookie(cname, cvalue, exdays) {
+	    const d = new Date();
+	    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	    let expires = "expires="+d.toUTCString();
+	    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	  }
+
+	  function getCookie(cname) {
+	    let name = cname + "=";
+	    let ca = document.cookie.split(';');
+	    for(let i = 0; i < ca.length; i++) {
+	      let c = ca[i];
+	      while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	      }
+	      if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	      }
+	    }
+	    return "";
+	  }
+
+	function checkCookie(uno) {
+	  let user = getCookie(uno);
+	  if (user != "") {
+	    return user;
+	  } else {
+	    user = prompt("what should be the the leter for:" + Uno, "");
+	    if (user != "" && user != null) {
+	      setCookie(uno, user, 365);
+	    }
+	  }
+	}
+function police(text){
+  text.replace(text, checkCookie(text));
+}
+function clicked(letter) {
+  document.getElementById(letter).addEventListener("click", () => {
+	  alert(police(letter));
+  }
+});
